0861fcb049370f838be54aa66d44ec94b5bb4b70
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2022-09-22 22:18:37 -0300

    first commit

---
+++ quest.js
@@ -0,0 +1,4 @@
+const question = {"enunciado":"texto livre ... ", "alternativas":{"a":"alternativa a ","b":"alternativa b","c":"alternativa c","d":"alternativa d"},
+"correta":"a", "frequencia":3, "acertos": 2, "erros":1}
+
+console.log(question.acertos);
